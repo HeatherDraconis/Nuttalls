7bee9c8cab0f91e304f811128a1388f6900f6976
HeatherDraconis <heather.nuttall@hotmail.com>
2025-05-01 15:08:28 +0100

    Initial upload of website

---
+++ articles/articles.js
@@ -0,0 +1,8 @@
+var articles = new Array(
+"<a href= 'articles/switzerland.htm'>Switzerland</a>",
+"<a href= 'articles/corsica.htm'>Corsica</a>",
+"<a href= 'articles/dodecanese.htm'>Dodecanese</a>",
+"<a href= 'articles/madeira.htm'>madeira</a>",
+"<a href= 'articles/minorca.htm'>Minorca</a>",
+"<a href= 'articles/alpbach.htm'>Alpbach</a>"
+);
